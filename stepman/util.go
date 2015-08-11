14fe2a0a9a844b3a4f5bec7c1599e96650eaa2c8
Krisztian Goedrei <godreikrisztian@gmail.com>
2015-08-11 18:12:23 +0200

    downloadAndUnzip quick fix

--- stepman/util.go
+++ stepman/util.go
@@ -1,13 +1,10 @@
 package stepman
 
 import (
-	"archive/zip"
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
 	"io/ioutil"
-	"net/http"
 	"os"
 	"path"
 	"path/filepath"
@@ -92,7 +89,7 @@ func DownloadStep(collection models.StepCollectionModel, id, version, commithash
 		switch downloadLocation.Type {
 		case "zip":
 			log.Debug("[STEPMAN] - Downloading step from:", downloadLocation.Src)
-			if err := DownloadAndUnZIP(downloadLocation.Src, stepPth); err != nil {
+			if err := cmdex.DownloadAndUnZIP(downloadLocation.Src, stepPth); err != nil {
 				log.Warn("[STEPMAN] - Failed to download step.zip:", err)
 			} else {
 				success = true
@@ -290,104 +287,6 @@ func ReGenerateStepSpec(route SteplibRoute) error {
 
 	if err := WriteStepSpecToFile(collection, route); err != nil {
 		return err
-	}
-	return nil
-}
-
-// DownloadAndUnZIP ...
-func DownloadAndUnZIP(url, pth string) error {
-	filePath := os.TempDir() + "step.zip"
-	file, err := os.Create(filePath)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		if err := file.Close(); err != nil {
-			log.Fatal("Failed to close file:", err)
-		}
-		if err := os.Remove(filePath); err != nil {
-			log.Fatal("Failed to remove file:", err)
-		}
-	}()
-
-	response, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		if err := response.Body.Close(); err != nil {
-			log.Fatal("Failed to close response body:", err)
-		}
-	}()
-
-	if response.StatusCode != http.StatusOK {
-		errorMsg := "Failed to download step.zip from: " + url
-		return errors.New(errorMsg)
-	}
-
-	log.Info("Successfully downloaded step.zip")
-	if _, err := io.Copy(file, response.Body); err != nil {
-		return err
-	}
-
-	return unzip(filePath, pth)
-}
-
-func unzip(src, dest string) error {
-	r, err := zip.OpenReader(src)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		if err := r.Close(); err != nil {
-			log.Fatal(err)
-		}
-	}()
-
-	if err := os.MkdirAll(dest, 0755); err != nil {
-		return err
-	}
-
-	// Closure to address file descriptors issue with all the deferred .Close() methods
-	extractAndWriteFile := func(f *zip.File) error {
-		rc, err := f.Open()
-		if err != nil {
-			return err
-		}
-		defer func() {
-			if err := rc.Close(); err != nil {
-				log.Fatal(err)
-			}
-		}()
-
-		path := filepath.Join(dest, f.Name)
-
-		if f.FileInfo().IsDir() {
-			if err := os.MkdirAll(path, f.Mode()); err != nil {
-				return err
-			}
-		} else {
-			f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-			if err != nil {
-				return err
-			}
-			defer func() {
-				if err := f.Close(); err != nil {
-					log.Fatal(err)
-				}
-			}()
-
-			if _, err = io.Copy(f, rc); err != nil {
-				return err
-			}
-		}
-		return nil
-	}
-
-	for _, f := range r.File {
-		if err := extractAndWriteFile(f); err != nil {
-			return err
-		}
 	}
 	return nil
 }
