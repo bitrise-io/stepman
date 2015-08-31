dae9a0984cbedf3a370d4b2eebf2cf6d4790de39
Viktor Benei <viktor.benei@gmail.com>
2015-08-31 17:44:50 +0200

    start of v0.9.13

--- cli/cli.go
+++ cli/cli.go
@@ -52,7 +52,7 @@ func Run() {
 	app := cli.NewApp()
 	app.Name = path.Base(os.Args[0])
 	app.Usage = "Step manager"
-	app.Version = "0.9.12"
+	app.Version = "0.9.13"
 
 	app.Author = ""
 	app.Email = ""
