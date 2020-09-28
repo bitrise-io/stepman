c17099b5d8738d0171a11808a1553abacee12099
zsolt-marta-bitrise <zsolt.marta@bitrise.io>
2020-09-28 11:02:30 +0200

    [PLAT-92] Generate step-info.yml for new steps (#295)
    * [PLAT-92] Renaming and minor grammar fixes
    
    * [PLAT-92] more accurate naming
    
    * [PLAT-92] Create step-info.yml if this is a new step
    
    * [PLAT-92] Add new step-info.yml to git stage
    
    * [PLAT-92] Wait until step is integrated till we generate the group spec
    
    * [PLAT-92] Git status should include untracked files (like the step-info.yml)
    
    * [PLAT-92] Actually add step info yml to commit
    
    * [PLAT-92] spec is not read fully in this stage, use the directory's existence as a marker for existing step
    
    * [PLAT-92] Unused method
    
    * [PLAT-92] Add doc on new exported func
    
    * [PLAT-92] Tests for paths
    
    * [PLAT-92] Lint fix
    
    * [PLAT-92] errcheck
    
    * [PLAT-92] Add test for default spec
    
    * [PLAT-92] Use filepath instead of path when joining paths
    
    * [PLAT-92] Extract to separate method
    
    * [PLAT-92] Use camelCase in const, proper doc
    
    * [PLAT-92] Reset home after test
    
    * [PLAT-92] Go expects the expected var first
    
    * [PLAT-92] errcheck

---
+++ stepman/paths_test.go
@@ -0,0 +1,101 @@
+package stepman
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+const (
+	givenSteplibURI  = "https://github.com/bitrise-io/steplib"
+	givenFolderAlias = "12334556"
+	givenHomePath    = "/usr/testeruser/"
+	givenStepID      = "test-custom-step"
+	givenStepVersion = "0.5.6"
+)
+
+func GivenRoute() SteplibRoute {
+	return SteplibRoute{
+		SteplibURI:  givenSteplibURI,
+		FolderAlias: givenFolderAlias,
+	}
+}
+
+func Test_GivenHomeDir_WhenGetStepmanDirPathCalled_ThenGoodPathReturned(t *testing.T) {
+	// Given
+	oldhome := os.Getenv("HOME")
+	err := os.Setenv("HOME", givenHomePath)
+	defer func() {
+		err := os.Setenv("HOME", oldhome)
+		require.NoError(t, err)
+	}()
+	require.NoError(t, err)
+
+	expected := filepath.Join(givenHomePath, ".stepman")
+
+	// When
+	actual := GetStepmanDirPath()
+
+	// Then
+	assert.Equal(t, expected, actual)
+}
+
+func Test_GivenStepmanDir_WhenGetCollectionDirPathCalled_ThenGoodPathReturned(t *testing.T) {
+	// Given
+	oldhome := os.Getenv("HOME")
+	err := os.Setenv("HOME", givenHomePath)
+	defer func() {
+		err := os.Setenv("HOME", oldhome)
+		require.NoError(t, err)
+	}()
+	require.NoError(t, err)
+
+	expected := filepath.Join(GetStepmanDirPath(), "step_collections")
+
+	// When
+	actual := GetCollectionsDirPath()
+
+	// Then
+	assert.Equal(t, expected, actual)
+}
+
+func Test_GivenRoute_WhenGetLibraryBaseDirPathCalled_ThenGoodPathReturned(t *testing.T) {
+	// Given
+	route := GivenRoute()
+	expected := filepath.Join(GetCollectionsDirPath(), route.FolderAlias, "collection")
+
+	// When
+	actual := GetLibraryBaseDirPath(route)
+
+	// Then
+	assert.Equal(t, expected, actual)
+}
+
+func Test_GivenRouteAndStepId_WhenGetStepCollectionDirPath_ThenGoodPathReturned(t *testing.T) {
+	// Given
+	route := GivenRoute()
+	step := givenStepID
+	version := givenStepVersion
+	expected := filepath.Join(GetLibraryBaseDirPath(route), "steps", step, version)
+
+	// When
+	actual := GetStepCollectionDirPath(route, step, version)
+
+	// Then
+	assert.Equal(t, expected, actual)
+}
+
+func Test_GivenRouteAndStepId_WhenGetStepGlobalInfoPathCalled_ThenGoodPathReturned(t *testing.T) {
+	// Given
+	route := GivenRoute()
+	step := givenStepID
+	expected := filepath.Join(GetLibraryBaseDirPath(route), "steps", step, "step-info.yml")
+
+	// When
+	actual := GetStepGlobalInfoPath(route, step)
+
+	// Then
+	assert.Equal(t, expected, actual)
+}
