060cd99038faf89656db316e04792ca4e1955f2c
Laszlo Pusok <laszlo.pusok@bitrise.io>
2022-08-05 11:33:52 +0200

    Fixed version string overwrite

--- version/version.go
+++ version/version.go
@@ -1,4 +1,4 @@
 package version
 
-// VERSION is the stepman version number. It's defined at build time using -ldflags
-const VERSION = "0.0.0-development"
+// Version is the stepman version number. It's defined at build time using -ldflags
+var Version = "0.0.0-development"
